fbf0902528e73c1b36038562e043620c1bff75cf
Joao Gilberto <joaomonteirovaz@protonmail.com>
2024-08-19 14:45:26 -0300

    incluindo player no connect4

--- include/Connect4.hpp
+++ include/Connect4.hpp
@@ -2,14 +2,17 @@
 #define CONNECT4_H
 
 #include "Connect4Board.hpp"
+#include "Player.h"
+#include "PlayerManager.h"
 
 class Ligue4 {
 private:
     Board4 board;
     int lastMoveX, lastMoveY;
+    Player* activePlayers[2];
 
 public:
-    Ligue4(int col, int row);
+    Ligue4(int col, int row, const string& nickname1, const string& nickname2);
 };
 
 #endif
--- src/Connect4.cpp
+++ src/Connect4.cpp
@@ -1,6 +1,19 @@
 #include "Connect4.hpp"
 
-Ligue4::Ligue4(int col, int row) : board(col, row) {
+Ligue4::Ligue4(int col, int row, const string& nickname1, const string& nickname2) : board(col, row) {
+
+    PlayerManager pm; 
+    
+    Player player1 = pm.returnPlayerByNickname(nickname1);
+    Player player2 = pm.returnPlayerByNickname(nickname2);
+    
+    if (player1.getNickname().empty() || player2.getNickname().empty()) {
+        throw std::invalid_argument("ERRO: Um ou ambos os jogadores não foram encontrados.");
+    }
+
+    // Inicializa os ponteiros para os jogadores ativos
+    activePlayers[0] = new Player(player1);
+    activePlayers[1] = new Player(player2);
 
     cout << "Por favor, selecione um número de 1 a " << row << endl;
     for (int i = 1; i <= row; ++i) {
@@ -13,14 +26,25 @@ Ligue4::Ligue4(int col, int row) : board(col, row) {
         board.readMove(1, lastMoveX, lastMoveY);
         board.printBoard();
         if (board.checkWinner(lastMoveX, lastMoveY, 1)) {
-            cout << "\nVocê ganhou!" << endl;
+
+            Player& winner = *activePlayers[0];
+            Player& loser = *activePlayers[1];
+
+            cout << "\n" << winner.getNickname() <<"Ganhou!" << endl;
             break;
         }
 
         board.readMove(2, lastMoveX, lastMoveY);
         board.printBoard();
         if (board.checkWinner(lastMoveX, lastMoveY, 2)) {
-            cout << "\nVocê ganhou!" << endl;
+            Player& loser = *activePlayers[0];
+            Player& winner = *activePlayers[1];
+            cout << "\n" << winner.getNickname() <<"Ganhou!" << endl;
+            winner.updateLig4PlayerStats(winner.getNumOfVictoriesLig4() + 1, winner.getNumOfLossesLig4());
+            loser.updateLig4PlayerStats(loser.getNumOfVictoriesLig4(), loser.getNumOfLossesLig4() + 1 );
+            PlayerManager pm;
+            pm.updatePlayer(winner.getNickname() , winner);
+            pm.updatePlayer(loser.getNickname(), loser);
             break;
         }
     }
