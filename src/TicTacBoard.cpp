c42fbd1930026d9613a41e5d138f33250378d1ba
Joao Gilberto <joaomonteirovaz@protonmail.com>
2024-08-19 15:57:49 -0300

    correção do TicTacBoard

--- src/TicTacBoard.cpp
+++ src/TicTacBoard.cpp
@@ -1,65 +1,70 @@
 #include "TicTacToe.hpp"
-#include "../include/Player.h"
-#include "../include/PlayerManager.h"
-#include "../include/Utilitiies.h"
 
-TicTacToe::TicTacToe(const Board& board) : board_(board) {}
+void Board::startBoard() {
 
-TicTacToe::~TicTacToe() {
-    delete activePlayers[0];
-    delete activePlayers[1];
+    int position= 1;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board_[i][j] = POSITIONS[position++];
+        }
+    }
 }
 
-void TicTacToe::Start(const string& nickname1, const string& nickname2) {
-    PlayerManager pm; 
-    
-    Player player1 = pm.returnPlayerByNickname(nickname1);
-    Player player2 = pm.returnPlayerByNickname(nickname2);
-    
-    if (player1.getNickname().empty() || player2.getNickname().empty()) {
-        throw std::invalid_argument("ERRO: Um ou ambos os jogadores não foram encontrados.");
+void Board::printBoard() {
+    cout << endl;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << "| " << board_[i][j] << " ";
+        }
+        cout << "| \n";
+    }
+}
+
+bool Board::checkWin() {
+    int i = 0;
+    int j = 0;
+
+    //checar linhas
+    for(int i = 0; i < 3; i++){
+        if (board_[i][j] == board_[i][j++] && board_[i][j] == board_[i][j++]) {
+            return true;
+        }
+        j = 0;
     }
 
-    // Inicializa os ponteiros para os jogadores ativos
-    activePlayers[0] = new Player(player1);
-    activePlayers[1] = new Player(player2);
-
-    board_.startBoard();
-
-    int moves = 0;
-    while (moves < 9) {
-        cout << "JOGO DA VELHA" << endl;
-        board_.printBoard();
-        cout << "Turno do jogador " << (moves % 2 ? nickname1 : nickname2) << endl;
-        int position = 0;
-        cin >> position;
-        while (position > 9 || position < 1) {
-            cout << "Posicao inválida. Selecione um número de 1 a 9.";
-            cin >> position;
-        };
-
-        while (!board_.updatePosition(position)) {
-            cout << "Posição inválida. Tente novamente com uma posição válida.";
+    //checar colunas
+    for(int j = 0; j < 3; j++){
+        if (board_[i][j] == board_[++i][j] && board_[i][j]== board_[++i][j]) {
+            return true;    
         }
-       
-        if (moves > 3) {
-            if(board_.checkWin()) {
-                board_.checkWin();
-                cout << "Jogador " << (moves % 2 ? nickname1 : nickname2) << " ganhou!" << endl;
+        i = 0;
+    }
 
-                Player& winner = moves % 2 ? *activePlayers[0] : *activePlayers[1];
-                Player& loser = moves % 2 ? *activePlayers[1] : *activePlayers[0];
-                winner.updateTicTacToePlayerStats(winner.getNumOfVictoriesTicTacToe() + 1, winner.getNumOfLossesTicTacToe());
-                loser.updateTicTacToePlayerStats(loser.getNumOfVictoriesTicTacToe(), loser.getNumOfLossesTicTacToe() + 1 );
-                PlayerManager pm;
-                pm.updatePlayer(winner.getNickname(), winner);
-                pm.updatePlayer(loser.getNickname(), loser);
+    //checar diagonais
+    if (board_[i][j] == board_[++i][++j] && board_[i][j] == board_[++i][++j]) {
+        return true; 
+    }
 
-                break;
-            } else if (moves == 8) {
-                cout << "EMPATE!" << endl;
+    i = 0;
+    j = SIZE - 1;
+    return board_[i][j] == board_[++i][--j] && board_[i][j] == board_[++i][--j];
+}
+
+bool Board::updatePosition(int position) {
+    bool update = false;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board_[i][j] == POSITIONS[position]) {
+                board_[i][j] = mark;
+                update = true;
             }
-       }
-       moves++;
-    };
+        }
+    }
+    if (mark == 'X') {
+        mark = 'O';
+    }else {
+        mark = 'X';    
+    }
+
+    return update;
 }
